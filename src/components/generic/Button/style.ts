6152734efd0af591da8b7fb0b0d853890e6bda86
NinaBredova <innawonkaarm@gmail.com>
2023-02-09 13:39:53 +0400

    Add styles for button

--- src/components/generic/Button/style.ts
+++ src/components/generic/Button/style.ts
@@ -1,6 +1,31 @@
 import styled from '@emotion/styled'
 
 //types
-import type { ButtonContainerProps } from './types'
+import { ButtonContainerProps, ButtonSizes, ButtonVariants} from './types'
 
-export const ButtonContainer = styled.button<ButtonContainerProps>``
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: ${({ size }) =>
+    size === ButtonSizes.s ? '40px' : size === ButtonSizes.m ? '48px' : 'auto'};
+
+  color: ${({ theme, variant }) =>
+    variant === ButtonVariants.primary};
+
+  font-weight: 700;
+
+  ${({ width }) => (!width ? null : `width: ${width}`)};
+  ${({ margin }) => (!margin ? null : `margin: ${margin}`)};
+
+  cursor: pointer;
+
+
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+
+`
