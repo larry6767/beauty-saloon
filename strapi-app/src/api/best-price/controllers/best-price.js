e587aca363bb0d71d68f84c6721eecf5d3d9fda2
NinaBredova <innawonkaarm@gmail.com>
2023-02-17 15:23:28 +0400

    add data in the best prices

---
+++ strapi-app/src/api/best-price/controllers/best-price.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * best-price controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::best-price.best-price');
---
+++ strapi-app/src/api/submit-your-application/routes/submit-your-application.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * submit-your-application router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::submit-your-application.submit-your-application');
