41fb516c8302bf7944a12812f80645cd973d8719
NinaBredova <innawonkaarm@gmail.com>
2023-02-24 21:02:00 +0400

    add lost strapi data for best prices

--- strapi-app/src/api/best-price-image/controllers/best-price-image.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/**
- * best-price-image controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories
-
-module.exports = createCoreController('api::best-price-image.best-price-image')
