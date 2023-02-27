51aa4e23a808d1411add2946e0b3cedd7aff5cea
Illarion Glebov <i.glebov@idynsys.org>
2023-02-27 14:40:08 +0100

    fix slider

--- src/middleware.ts
+++ src/middleware.ts
@@ -51,5 +51,8 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next).*)',
+  ],
 }
